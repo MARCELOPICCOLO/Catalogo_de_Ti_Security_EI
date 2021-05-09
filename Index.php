f647b90fd422bce4173090082a9493a7279c0206
MARCELOPICCOLO <marcelociabatari@hotmail.com>
2021-05-09 11:31:46 -0300

    resolvido bugs, painel de suporte e plano de continuidade ok

--- Index.php
+++ Index.php
@@ -5,27 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catálogo de TI da Security E&I</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="site.js"></script>
-    <link rel="stylesheet" href="style.css?v=2">
+    <link rel="stylesheet" type="text/css" href="./estilo.css?v=5"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
 <div class="container">
-    <h3>Catáligo de TI da Security E&I</h3>
     <div class="barra-titulos">Equipe de TI da Security E&I
         <button type="button" id="btnProf" class="but" onClick="ShowPainel(0)"></button>
     </div>
     <hr/>
-    <div  id="prof" class="profis">
-    </div>
+    <div  id="prof" class="profis"></div>
     <div class="barra-titulos">Suporte Técnico
         <button type="button" id="btnSup" class="but" onClick="ShowPainel(1)"></button>
     </div>
     <hr/>
     <div id="divSuporte" class="suporte"></div>
+
+    <div class="barra-titulos">Plano de Continuídade
+        <button type="button" id="btnPc" class="but" onClick="ShowPainel(2)"></button>
+    </div>
+    <hr/>
+    <div id="divPC" class="suporte"></div>
     
 </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="site.js?v=3"></script>
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 </body>
 </html>
--- site.js
+++ site.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
 
     loadEquipe();
     loadList();
-})
+    lodSecurity();
+    $("#aberturaemailpopupOpenInfo").hide();
+});
 
 let Show = false;
 let showDesc = false;
+let states = [false,false,false];
 
 
 function hidePainel(name){
@@ -17,7 +20,6 @@ function offLed(button){
 }
 
 function showPainel(name){
-    console.log(name);
     $(name).show(3.0);
 }
 
@@ -32,23 +34,40 @@ function ShowPainel(val){
 
     switch(val){
         case 0:
-            if(!Show){
+            if(!states[0]){
+                
                 hidePainel("#prof");
                 offLed("#btnProf");
             }else{
                 showPainel("#prof");
                 onLed("#btnProf");
             }
+            states[0]=Show;
         break;
 
         case 1:
-            if(!Show){
+            if(!states[1]){
+              
                 hidePainel("#divSuporte");
                 offLed("#btnSup");
             }else{
                 showPainel("#divSuporte");
                 onLed("#btnSup");
             }
+            states[1]=Show;
+        break;
+        
+        case 2:
+            if(!states[2]){
+               
+                hidePainel("#divPC");
+                offLed("#btnPc");
+            }else{
+                showPainel("#divPC");
+                onLed("#btnPc");
+            }
+            states[2]=Show;
+
         break;
     } 
 }
@@ -57,13 +76,12 @@ function ShowPainel(val){
 function onOffDesc(val){
  
     showDesc = !showDesc;
-    console.log(showDesc);
    
     if(!showDesc){
-        $('#d'+val+'').hide();
+        $('#'+val[0]+val+'').hide();
     }
     else{
-        
+        $('#'+val[0]+val+'').show();
     }
 }
 
@@ -110,7 +128,7 @@ function loadList(){
   var list = [{
                "Ordem" : "Manutenção de Equipamentos",
                "icon"  : '<i class="fas fa-tools" style="font-size:36px"></i>',
-               "descricao"  : 'lorem aaaaaaaa......................',
+               "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
                "permitidos" : "aaaa",
               },
               {
@@ -125,19 +143,79 @@ function loadList(){
                 "descricao"  : 'lorem ..ccccccc........................',
                 "permitidos" : "aaaa",
               },
+              {
+                "Ordem" : "Instalar softwares",
+                "icon"  : '<i class="fas fa-cubes" style="font-size:36px"></i>',
+                "descricao"  : 'lorem ..ccccccc........................',
+                "permitidos" : "aaaa",
+              },
 
             ];
 
   var txt ="";
  
     $.each(list,function(index){
-        txt+='<div class="card2"><div style="display:flex"><div class="icon">'+list[index]["icon"]+'</div>'+list[index]["Ordem"]+'</div>';
-        txt+='<button type="button" id="d'+index+'" onclick="onOffDesc(this.id)">Exibir </button>';
+        txt+='<div class="card2"><div style="display:flex; justify-content:space-between;align-items:center"><div class="icon">'+list[index]["icon"]+'</div><h3>'+list[index]["Ordem"]+'</h3></div>';
+        txt+='<div" id="d'+index+'" style="background-color=#ccc"; width:100%; cursor:pointer" onclick="onOffDesc(this.id)"> <i class="fas fa-angle-down" style="font-size:24px"></i>';
         txt+='<hr/>';
-        txt+='<div id="dd'+index+'" class=painel><pre>'+list[index]["descricao"]+'</div></div>';   
+        txt+='<div id="dd'+index+'" class=painel>'+list[index]["descricao"]+'</div></div>';   
     })
 
    $("#divSuporte").html(txt);
+   hidePainels(`d`,list);
 }
 
+function lodSecurity(){
+    var txt="";
+    var list = [
+                    {
+                        "Ordem" : "Realizar Backup",
+                        "icon"  : '<i class="fas fa-database" style="font-size:36px"></i>',
+                        "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
+                        "permitidos" : "aaaa",                  
 
+                    },
+                    {
+                        "Ordem" : "Acessar a Extranet",
+                        "icon"  : '<i class="fas fa-globe" style="font-size:36px"></i>',
+                        "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
+                        "permitidos" : "aaaa",                  
+
+                    },
+                    {
+                        "Ordem" : "Centralizar em Nuvem",
+                        "icon"  : '<i class="fas fa-cloud" style="font-size:36px"></i>',
+                        "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
+                        "permitidos" : "aaaa",                  
+
+                    },
+                    {
+                        "Ordem" : "Estruturar Datacenter",
+                        "icon"  : '<i class="fas fa-server" style="font-size:36px"></i>',
+                        "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
+                        "permitidos" : "aaaa",                  
+
+                    },
+            
+            ];
+
+    
+    $.each(list,function(index){
+        txt+='<div class="card2"><div style="display:flex; justify-content:space-between;align-items:center; padding: 10px 20px"><div class="icon">'+list[index]["icon"]+'</div><h3>'+list[index]["Ordem"]+'</h3></div>';
+        txt+='<div" id="c'+index+'" style="background-color=#ccc"; width:100%; cursor:pointer" onclick="onOffDesc(this.id)"> <i class="fas fa-angle-down" style="font-size:24px"></i>';
+        txt+='<hr/>';
+        txt+='<div id="cc'+index+'" class=painel>'+list[index]["descricao"]+'</div></div>';  
+    })
+    $("#divPC").html(txt);
+
+    hidePainels(`c`,list);
+
+}
+
+function hidePainels(val,list){
+    console.log(val);
+    $.each(list,function(index){
+       
+        $('#'+val+val+index+'').hide(3.0);
+    })
+}
