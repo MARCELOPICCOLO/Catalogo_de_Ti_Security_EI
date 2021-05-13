ad182e94900c1d8b9ec40c16b47e40f9d1d55632
MARCELOPICCOLO <marcelociabatari@hotmail.com>
2021-05-13 15:54:49 -0300

    projeto finalizado

--- Index.php
+++ Index.php
@@ -5,13 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catálogo de TI da Security E&I</title>
-    <link rel="stylesheet" type="text/css" href="./estilo.css?v=6"/>
+    <link rel="stylesheet" type="text/css" href="./estilo.css?v=13"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
 <div class="container">
+    <div  class="banner">
+        <i class='fas fa-shield-alt' style='font-size:80px; color:rgb(206, 194, 85)'></i>
+        <h1>SECURTY E&I</h1>
+    </div>
+
+    <H2 style="text-align: center; margin-top:20px; font-size: 2.1rem;color:#555"> Catálogo de Serviços de TI</h2>
+
     <div class="barra-titulos">Equipe de TI da Security E&I
         <button type="button" id="btnProf" class="but" onClick="ShowPainel(0)"></button>
     </div>
@@ -34,6 +41,19 @@
     </div>
     <hr/>
     <div id="divSeg" class="suporte"></div>
+
+    <div class="barra-titulos">Novas Soluções
+        <button type="button" id="btnSol" class="but" onClick="ShowPainel(4)"></button>
+    </div>
+    <hr/>
+    <div id="divSol" class="suporte"></div>
+
+    <div  class="footer">
+        <li>Marcelo Ciabatari Piccolo</li>
+        <li>Larissa Camila Santos de Lima</li>
+        <li> Antônio Marcelino Alves Dos Santos</li>
+        <li> João vitor Batista Marques</li>
+    </div>
     
 </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
--- site.js
+++ site.js
@@ -3,26 +3,29 @@
 var suporte = [{
     "Ordem" : "Manutenção de Equipamentos",
     "icon"  : '<i class="fas fa-tools" style="font-size:36px"></i>',
-    "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
-    "permitidos" : "aaaa",
+    "descricao"  : '<p>Por motivo de segurança, a TI ficará a cargo de prestar manutenção nos equipamentos internos descrito nos itens:'
+                    + '<li>Trocar todas as placas e dispositivos</li><li>Instalar novos equipamentos</li></p>',
+                    
+    "permitidos" : "Técnico de Informática",
    },
    {
      "Ordem" : "Atualização de softwares",
      "icon"  : '<i class="fas fa-redo-alt" style="font-size:36px"></i>',
-     "descricao"  : 'lorem .bbbbbbb.........................',
-     "permitidos" : "aaaa",
+     "descricao"  : '<p>Fica a cargo da TI atualizar os softwares e licenças dos softwares internos.</p>',
+     "permitidos" : "Técnico de Informática",
    },
    {
      "Ordem" : "Configuração de rede",
      "icon"  : '<i class="fas fa-wifi" style="font-size:36px"></i>',
-     "descricao"  : 'lorem ..ccccccc........................',
-     "permitidos" : "aaaa",
+     "descricao"  : '<p>Fica a cargo da Ti todo o gerenciamento da comunicação interna a cooporação como:'
+                    +'<li>Configurar endereço dos equipamentos na rede</li><li>Adicionar ou remover equipamentos</p></li>',
+     "permitidos" : "Técnico de Informática",
    },
    {
      "Ordem" : "Instalar softwares",
      "icon"  : '<i class="fas fa-cubes" style="font-size:36px"></i>',
-     "descricao"  : 'lorem ..ccccccc........................',
-     "permitidos" : "aaaa",
+     "descricao"  : '<p>Para garantir a segurança da coorporações, deverá ser utilizado softwares originais</p>',
+     "permitidos" : "Técnico de Informática",
    },
 
  ];
@@ -31,22 +34,23 @@ var suporte = [{
     {
         "Ordem" : "Realizar Backup",
         "icon"  : '<i class="fas fa-database" style="font-size:36px"></i>',
-        "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
-        "permitidos" : "aaaa",                  
+        "descricao"  : '<p>A Ti deverá realizar o backup diariamente após o término do expediente, o backup deverá ser guardado em mídias não volatéis e armazenamento em nuvens</p>',
+        "permitidos" : "DBA, analistas",                  
 
     },
     {
         "Ordem" : "Centralizar em Nuvem",
         "icon"  : '<i class="fas fa-cloud" style="font-size:36px"></i>',
-        "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
-        "permitidos" : "aaaa",                  
+        "descricao"  : '<p>O sistema central deverá estar centralizado nas nuvens cabendo a Ti garantir a segurança e funcionamento</p>.',
+        "permitidos" : "TI em geral",                  
 
     },
     {
         "Ordem" : "Estruturar Datacenter",
         "icon"  : '<i class="fas fa-server" style="font-size:36px"></i>',
-        "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
-        "permitidos" : "aaaa",                  
+        "descricao"  : '<p>A Ti cuidará da segurança dos dados coletados, o banco de dados deverá seguir os itmens:'
+                    + '<li>Estar configurado em um local seguro a acidentes naturais</li><li>conter dispositivo anti-chamas</li><li>possír cópias de segurança armazenado em nuvem</p></li>',
+        "permitidos" : "TI em geral",                  
 
     },
 
@@ -56,25 +60,37 @@ var security = [
     {
         "Ordem" : "Configurar Firewall",
         "icon"  : '<i class="fas fa-shield-alt" style="font-size:36px"></i>',
-        "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
-        "permitidos" : "aaaa",                  
+        "descricao"  : '<p>Cabe a TI zelar pela segurança da comeunicação da coorporação, onde deverá proteger os equipamentos com firewall e anti malwares.</p>',
+        "permitidos" : "CSO, analistas",                  
 
     },
     {
         "Ordem" : "Passar Anti-vírus",
         "icon"  : '<i class="fas fa-bug" style="font-size:36px"></i>',
-        "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
-        "permitidos" : "aaaa",                  
+        "descricao"  : '<p>Cabe a TI garantir a segurança dos equipamentos da coorporação, seguindo os itens:'
+                       +'<li>Passar anti-virus 2 vezes por semanas em horários agendados</li><li>Passar anti-virus em todas as mídias não volatés</li></p>',
+        "permitidos" : "Analistas, técnico",                  
 
     },
     {
         "Ordem" : "Gerenciar Acessos a rede",
         "icon"  : '<i class="fas fa-exclamation-triangle" style="font-size:36px"></i>',
-        "descricao"  : 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
-        "permitidos" : "aaaa",                  
+        "descricao"  : '<p>A Ti deverá cuidar dos acessos realizados internamente, permitindo ou restringindo acessos por meio de colaboradores</p>',
+        "permitidos" : "CSO",                  
 
     },
 
+
+];
+
+var solutions = [
+    {
+        "Ordem" : "Implantar novas soluções",
+        "icon"  : '<i class="fas fa-lightbulb" style="font-size:36px"></i>',
+        "descricao"  : '<p>A TI deverá ficar a cargo das necessidades da coorporação, e responsabilizará por:'
+                    +  '<li>levantar requisitos</li><li>Projetar novas soluções</li><li>Desenvoler e Implantar projetos<p></li>',
+        "permitidos" : "TI em geral",                  
+    },
 
 ];
 
@@ -93,9 +109,10 @@ $(document).ready(function(){
 
 function Initialize(){
     loadEquipe();
-    loadPainels(suporte,"#divSuporte","d");
-    loadPainels(plain,"#divPC","c");
-    loadPainels(security,"#divSeg","e");
+    loadPainels(suporte,"#divSuporte","a");
+    loadPainels(plain,"#divPC","b");
+    loadPainels(security,"#divSeg","c");
+    loadPainels(solutions,"#divSol","d");
 
     hidePainelsOrdens();
 }
@@ -179,6 +196,18 @@ function ShowPainel(val){
             onLed("#btnSg");
         }
       break;
+
+      case 4: 
+      if(!states[4]){
+             
+          hidePainel("#divSol");
+          offLed("#btnSol");
+      }else{
+          showPainel("#divSol");
+          onLed("#btnSol");
+      }
+
+      break;
     } 
 
     states[val] = Show;
@@ -206,32 +235,38 @@ function loadEquipe(){
     var txt="";
 
     var equipe = [ 
-                    {"nome" : "Amanda Vasconcellos Lima",
-                    "cargo" : "Analista de Sistemas Pleno ",
+                    {"nome" : "Amanda",
+                    "cargo" : "CSO (Chief Security Officer)",
                     "foto"  : "amanda.jpg"
                     },
-                    {"nome" : "David Santos ",
-                    "cargo" : "Analista de Sistemas Senior (CIO)",
+                    {"nome" : "David",
+                    "cargo" : "CIO (Chief Information Officer)",
                     "foto"  : "David.jpg"
                     },
-                    {"nome" : "Paloma Pereira sanches ",
-                    "cargo" : "Analista de Sistemas junior",
+                    {"nome" : "Paloma",
+                    "cargo" : "DBA (Database Admin)",
                     "foto"  : "paloma.jpg"
                     },
-                    {"nome" : "Jão Paulo  Bastos ",
-                    "cargo" : "Analista de Sistemas junior",
+                    {"nome" : "Jão Paulo",
+                    "cargo" : "Analista de Sistemas",
                     "foto"  : "Joao Paulo.jpg"
                     },
-                    {"nome" : "Letícia Alves ",
-                    "cargo" : "Analista de Sistemas junior",
+                    {"nome" : "Letícia",
+                    "cargo" : "Analista de Sistemas",
                     "foto"  : "leticia.jpg"
-                    },                         
+                    },
+                    {
+                    "nome" : "wellington ",
+                    "cargo" : "Analista e técnico de informática",
+                    "foto"  : "wellington.png"
+                    },
+                                            
                 ];
                 
                 
     $.each(equipe, function(index){
         txt+='<div class="card"><img src="./Assets/'+equipe[index]["foto"]+'"  style="width:100px; height:100px; border-radius: 50%">';
-        txt+='<div style="margin-left="20px"><h3>'+equipe[index]["nome"]+'</h3><p>'+equipe[index]["cargo"]+'</p></div></div>';
+        txt+='<div style="margin-left="20px"><h3>'+equipe[index]["nome"]+'</h3><p style="font-size:1.2rem">'+equipe[index]["cargo"]+'</p></div></div>';
     })
 
     $("#prof").hide(txt);
@@ -254,7 +289,8 @@ function loadPainels(list,nameDiv,varIdentify){
         txt+='<div class="card2"><div class="divImgTitle"><div class="icon">'+list[index]["icon"]+'</div><h3>'+list[index]["Ordem"]+'</h3></div>';
         txt+='<div" id="'+varIdentify+index+'" class="buttonDesc"  onclick="onOffDesc(this.id)"> <i class="fas fa-angle-down" style="font-size:24px"></i>';
         txt+='<hr/>';
-        txt+='<div id="'+varIdentify+varIdentify+index+'" class=painel>'+list[index]["descricao"]+'</div></div>';  
+        txt+='<div id="'+varIdentify+varIdentify+index+'" class=painel>'+list[index]["descricao"]+'</div>'
+              +'<p style="margin-top:10px"> Autorizado : '+list[index]["permitidos"]+'</div>';  
     })
     $(nameDiv).html(txt);
 
@@ -275,9 +311,12 @@ function hidePainels(val,list){
     })
 }
 
+/**Esconde os paineis */
+
 function hidePainelsOrdens(){
     $("#prof").hide();
     $("#divSuporte").hide();
     $("#divSeg").hide();
     $("#divPC").hide();
+    $("#divSol").hide();
 }
